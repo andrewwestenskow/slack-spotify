f3f9bc1e14552f41518fae97e64c876cd6d2f5a3
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-10-27 18:27:17 -0600

    Added keys

--- src/Components/SearchResults/SearchResults.jsx
+++ src/Components/SearchResults/SearchResults.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchResults = props => {
   const mappedResults = props.results.items.map(element => {
-    return <p>{element.name}</p>
+    return <p key={element.uri}>{element.name}</p>
   })
   return <div className="SearchResults">{mappedResults}</div>
 }

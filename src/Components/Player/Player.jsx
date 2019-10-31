7261f08934e4c5b3e41df2779d1e8b53c1f2dc29
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-10-30 21:51:42 -0600

    Try out color thing

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { togglePlay, nextTrack, previousTrack } from '../../functions/playback'
+import { connect } from 'react-redux'
 
 const Player = props => {
+  console.log(props)
   return (
     <div className="Player">
+      {props.current.album && (
+        <img
+          src={props.current.album.images[0].url}
+          alt=""
+          className="album-art"
+        />
+      )}
       <div className="control-button-hold">
         <button onClick={() => previousTrack(props.player)}>Previous</button>
         <button onClick={() => togglePlay(props.player)}>Play</button>
@@ -12,4 +21,9 @@ const Player = props => {
     </div>
   )
 }
-export default Player
+
+const mapStateToProps = state => {
+  return state.nowPlaying
+}
+
+export default connect(mapStateToProps)(Player)

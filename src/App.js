9ce672888e6564aa0166262f979e5d06f30db992
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-10-31 18:32:57 -0600

    Fixed loop

--- src/App.js
+++ src/App.js
@@ -7,38 +7,42 @@ import { connect } from 'react-redux'
 import { setAuth } from './ducks/authReducer'
 
 function App(props) {
-  useEffect(() => {
-    const handleRender = async () => {
-      if (props.access_token) {
+  const handleRender = async () => {
+    if (props.access_token) {
+      props.history.push('/auth/dashboard')
+    } else {
+      try {
+        const session = await axios.get('/session')
+        props.setAuth(session)
         props.history.push('/auth/dashboard')
-      } else {
-        try {
-          const session = await axios.get('/session')
-          props.setAuth(session)
-          props.history.push('/auth/dashboard')
-        } catch (error) {
-          const access_token = localStorage.getItem('access_token')
-          const refresh_token = localStorage.getItem('refresh_token')
-          if (access_token && refresh_token) {
-            try {
-              const { data: refresh } = await axios.post('/token', {
-                access_token,
-                refresh_token,
-              })
-              props.setAuth(refresh)
-              props.history.push('/auth/dashboard')
-            } catch (error) {
-              props.history.push('/')
-            }
-          } else {
+      } catch (error) {
+        const access_token = localStorage.getItem('access_token')
+        const refresh_token = localStorage.getItem('refresh_token')
+        if (access_token && refresh_token) {
+          try {
+            const refresh = await axios.post('/token', {
+              access_token,
+              refresh_token,
+            })
+            props.setAuth(refresh)
+            props.history.push('/auth/dashboard')
+          } catch (error) {
             props.history.push('/')
           }
+        } else {
+          props.history.push('/')
         }
       }
     }
+  }
+  useEffect(() => {
     handleRender()
     // eslint-disable-next-line
-  }, [])
+  }, [props.access_token])
+
+  window.onbeforeunload = () => {
+    handleRender()
+  }
 
   return <div className="App">{routes}</div>
 }

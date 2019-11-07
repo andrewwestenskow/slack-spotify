2c37c7e746133b7da133635a52ad13891e2693e7
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-11-07 11:26:19 -0700

    Play widget works for all

--- src/Components/Dashboard/FeaturedResult.jsx
+++ src/Components/Dashboard/FeaturedResult.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import { playPlaylist } from '../../functions/playback'
+import PlayWidget from '../PlayWidget/PlayWidget'
 
 const FeaturedResult = props => {
   const { info } = props
   return (
     <div className="result-hold">
       <div
-        onClick={() =>
-          playPlaylist(props.access_token, props.deviceId, info.uri, 0)
-        }
         style={{ backgroundImage: `url(${info.images[0].url})` }}
         className="result"
-      ></div>
+      >
+        <PlayWidget
+          access_token={props.access_token}
+          deviceId={props.deviceId}
+          uri={info.uri}
+          type="playlist"
+          offset={0}
+        />
+      </div>
       <div className="result-details">
         <p className="result-details-text">{info.name}</p>
       </div>
--- src/Components/Dashboard/TopArtistResult.jsx
+++ src/Components/Dashboard/TopArtistResult.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import { playArtist } from '../../functions/playback'
+import PlayWidget from '../PlayWidget/PlayWidget'
 
 const TopArtistResult = props => {
   const { info } = props
   return (
     <div className="result-hold">
       <div
-        onClick={() => playArtist(props.access_token, props.deviceId, info.uri)}
         style={{ backgroundImage: `url(${info.images[0].url})` }}
         className="result top-artist-result"
-      ></div>
+      >
+        <PlayWidget
+          access_token={props.access_token}
+          deviceId={props.deviceId}
+          type="artist"
+          uri={info.uri}
+        />
+      </div>
       <div className="result-details">
         <p className="result-details-text">{info.name}</p>
       </div>

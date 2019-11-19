a5ecf14f4f24609f1c9d4d4548cbc4e89b7bb801
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-11-19 10:30:31 -0700

    Refactored player controls

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -1,34 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import {
-  togglePlayback,
-  nextTrack,
-  previousTrack,
-  seek,
-} from '../../functions/playback'
 import { connect } from 'react-redux'
 import analyze from 'rgbaster'
 import spotify from '../../assets/spotify.png'
-import * as Icon from 'react-feather'
-import Slider from 'rc-slider'
-import 'rc-slider/assets/index.css'
-import { trackTime } from '../../functions/conversion'
+import PlayerControls from './PlayerControls'
 
 const Player = props => {
   const { playerState } = props
   const [gradient, setGradient] = useState('#000000')
-  const [position, setPosition] = useState(0)
-  const [seekInterval, setSeekInterval] = useState(0)
-  const [repeat, setRepeat] = useState(<></>)
-  const [repeatMode, setRepeatMode] = useState(0)
-  const [shuffle, setShuffle] = useState(false)
 
   const style = {
     background: `rgb(0,0,0)`,
     backgroundImage: `linear-gradient(312deg, rgba(0,0,0,1) 80%, ${gradient} 99%)`,
-  }
-
-  const handleSeek = e => {
-    seek(props.player, e)
   }
 
   useEffect(() => {
@@ -40,42 +22,6 @@ const Player = props => {
       )
     }
   }, [gradient, props])
-
-  useEffect(() => {
-    switch (repeatMode) {
-      case 0:
-        setRepeat(<Icon.Repeat className="control-button" />)
-        break
-      case 1:
-        setRepeat(<Icon.Repeat className="control-button" />)
-        break
-      case 2:
-        setRepeat(<Icon.Repeat className="control-button" />)
-        break
-      default:
-        setRepeat(<Icon.Repeat className="control-button" />)
-        break
-    }
-  }, [repeatMode])
-
-  useEffect(() => {
-    if (playerState.paused === false && !seekInterval && playerState.duration) {
-      console.log('NEW INTERVAL')
-      const newInterval = setInterval(() => {
-        props.player.getCurrentState().then(newState => {
-          if (newState) {
-            setPosition(newState.position)
-          }
-        })
-      }, 1000)
-      setSeekInterval(newInterval)
-    } else if (playerState.paused) {
-      clearInterval(seekInterval)
-      setSeekInterval(0)
-    }
-  }, [playerState, seekInterval, props.player])
-
-  console.log(playerState)
 
   return (
     <div className="Player" style={{ ...style }}>
@@ -96,47 +42,11 @@ const Player = props => {
       ) : (
         <div className="track-info-hold"></div>
       )}
-      <div className="player-controls-hold">
-        <div className="player-controls-slider-hold">
-          <p className="player-controls-slider-time">{trackTime(position)}</p>
-          <Slider
-            value={position}
-            min={0}
-            max={playerState.duration}
-            className="seek-bar-hold"
-            onAfterChange={e => handleSeek(e)}
-            onChange={e => setPosition(e)}
-            trackStyle={{ backgroundColor: '#18d860' }}
-            handleStyle={{ backgroundColor: '#18d860', border: 'none' }}
-          />
-          <p className="player-controls-slider-time">
-            {playerState.duration ? trackTime(playerState.duration) : '0:00'}
-          </p>
-        </div>
-        <div className="control-button-hold">
-          <Icon.Shuffle className="control-button" />
-          <Icon.SkipBack
-            className="control-button"
-            onClick={() => previousTrack(props.player)}
-          />
-          {props.playerState.paused || !props.current.name ? (
-            <Icon.Play
-              className="control-button"
-              onClick={() => togglePlayback(props.player)}
-            />
-          ) : (
-            <Icon.Pause
-              className="control-button"
-              onClick={() => togglePlayback(props.player)}
-            />
-          )}
-          <Icon.SkipForward
-            className="control-button"
-            onClick={() => nextTrack(props.player)}
-          />
-          {repeat}
-        </div>
-      </div>
+      <PlayerControls
+        current={props.current}
+        player={props.player}
+        playerState={playerState}
+      />
       <div className="volume-options-hold">
         <p>YUH</p>
       </div>
--- src/Components/Player/PlayerControls.jsx
+++ src/Components/Player/PlayerControls.jsx
@@ -1,6 +1,108 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
+import { trackTime } from '../../functions/conversion'
+import {
+  togglePlayback,
+  nextTrack,
+  previousTrack,
+  seek,
+} from '../../functions/playback'
+import * as Icon from 'react-feather'
 
 const PlayerControls = props => {
-  return <div></div>
+  const { playerState } = props
+  const [position, setPosition] = useState(0)
+  const [seekInterval, setSeekInterval] = useState(0)
+  const [repeat, setRepeat] = useState(<></>)
+  const [repeatMode, setRepeatMode] = useState(0)
+  const [shuffle, setShuffle] = useState(false)
+
+  const handleSeek = e => {
+    seek(props.player, e)
+  }
+
+  const handlePrevious = () => {
+    if (position < 5000) {
+      previousTrack(props.player)
+    } else {
+      seek(props.player, 0)
+    }
+  }
+
+  useEffect(() => {
+    if (playerState.paused === false && !seekInterval && playerState.duration) {
+      console.log('NEW INTERVAL')
+      const newInterval = setInterval(() => {
+        props.player.getCurrentState().then(newState => {
+          if (newState) {
+            setPosition(newState.position)
+          }
+        })
+      }, 1000)
+      setSeekInterval(newInterval)
+    } else if (playerState.paused) {
+      clearInterval(seekInterval)
+      setSeekInterval(0)
+    }
+  }, [playerState, seekInterval, props.player])
+
+  useEffect(() => {
+    switch (repeatMode) {
+      case 0:
+        setRepeat(<Icon.Repeat className="control-button" />)
+        break
+      case 1:
+        setRepeat(<Icon.Repeat className="control-button" />)
+        break
+      case 2:
+        setRepeat(<Icon.Repeat className="control-button" />)
+        break
+      default:
+        setRepeat(<Icon.Repeat className="control-button" />)
+        break
+    }
+  }, [repeatMode])
+
+  return (
+    <div className="Player-Controls">
+      <div className="player-controls-slider-hold">
+        <p className="player-controls-slider-time">{trackTime(position)}</p>
+        <Slider
+          value={position}
+          min={0}
+          max={playerState.duration}
+          className="seek-bar-hold"
+          onAfterChange={e => handleSeek(e)}
+          onChange={e => setPosition(e)}
+          trackStyle={{ backgroundColor: '#18d860' }}
+          handleStyle={{ backgroundColor: '#18d860', border: 'none' }}
+        />
+        <p className="player-controls-slider-time">
+          {playerState.duration ? trackTime(playerState.duration) : '0:00'}
+        </p>
+      </div>
+      <div className="control-button-hold">
+        <Icon.Shuffle className="control-button" />
+        <Icon.SkipBack className="control-button" onClick={handlePrevious} />
+        {props.playerState.paused || !props.current.name ? (
+          <Icon.Play
+            className="control-button"
+            onClick={() => togglePlayback(props.player)}
+          />
+        ) : (
+          <Icon.Pause
+            className="control-button"
+            onClick={() => togglePlayback(props.player)}
+          />
+        )}
+        <Icon.SkipForward
+          className="control-button"
+          onClick={() => nextTrack(props.player)}
+        />
+        {repeat}
+      </div>
+    </div>
+  )
 }
 export default PlayerControls
